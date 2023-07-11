e37475449225aec00b5861102e0f009c6f069d2d
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-07-11 19:55:47 -0400

    ran test on signup - working

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,7 +12,8 @@ router.post('/', async (req, res) => {
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.log(err);
+    res.status(400).json(err.message);
   }
 });
 
