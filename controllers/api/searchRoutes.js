be89305fa1d5a0a3a1848664cca4b56b164af6ff
jdelg140 <jacobdelgado140@gmail.com>
2023-07-12 19:31:36 -0400

    Changed the searchRoutes.js and homeRoutes.js files to use the api for cast of living

--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,7 +1,37 @@
-const router = require ('express').Router();
+const router = require('express').Router();
+const fetch = require('node-fetch');
 
-router.get('/:city', (req,res) => {
+router.get('/:city', async (req, res) => {
+  const cityName = req.params.city;
+  const url = 'https://cities-cost-of-living1.p.rapidapi.com/get_city_by_name';
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+      'X-RapidAPI-Host': 'cities-cost-of-living1.p.rapidapi.com',
+    },
+    body: new URLSearchParams({
+      name: cityName,
+    }),
+  };
 
-})
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
 
-module.exports=router;
+    //collect user financial information
+    const userIncome = req.query.income; // Extract income from query parameter or request body
+
+    // calculations or comparisons with the cost of living data and user's income
+
+
+    // Send the calculated data or relevant response back to the client
+    res.json({ city: cityName, costOfLivingData: result, userIncome });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+module.exports = router;
