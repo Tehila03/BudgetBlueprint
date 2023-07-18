803c3946ebf78db3ffa352031ce96e6902372995
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-07-18 19:21:28 -0400

    added search routes and updated user route connection

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const searchRoutes = require('./searchRoutes');
+const userRoutes = require('./userRoutes');
 
+router.use('/users',userRoutes);
 router.use('/search', searchRoutes);
 
 module.exports=router
--- controllers/api/searchRoutes.js
+++ controllers/api/searchRoutes.js
@@ -1,64 +1,20 @@
 const { Router } = require('express');
-
+const CostOfLiving = require('../../models');
 
 const router = Router();
-
-router.get('/', async (req, res) => {
-  const url = 'https://cities-cost-of-living1.p.rapidapi.com/get_cities_list';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'b7f78328f1msh650327bb4a94881p13a6d2jsn2e27c4098761',
-      'X-RapidAPI-Host': 'cities-cost-of-living1.p.rapidapi.com',
-    },
-  };
-
+router.get('/', async (req,res) => {
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+    const{city1, city2} = req.body
+    const city1Data = await CostOfLiving.findOne ({where:{city:city1}})
+    const city2Data = await CostOfLiving.findOne ({where:{city:city2}})
+    const homeCity = city1Data.get({plain:true})
+    const destinationCity = city2Data.get({plain:true})
+    res.status(200).json({homeCity,destinationCity})
   } catch (error) {
-    console.error(error);
+   res.status(500).json(error.message) 
   }
 
-  res.render('homepage', {
-    // Pass any data needed for rendering the homepage
-  });
-});
-
-router.post('/', async (req, res) => {
-  const { cities, currencies } = req.body;
-
-  const url = 'https://cities-cost-of-living1.p.rapidapi.com/get_cities_details_by_name';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-RapidAPI-Key': 'b7f78328f1msh650327bb4a94881p13a6d2jsn2e27c4098761',
-      'X-RapidAPI-Host': 'cities-cost-of-living1.p.rapidapi.com',
-    },
-    body: JSON.stringify({
-      cities,
-      currencies,
-    }),
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-
-    // Handle the result and extract the necessary data for rendering
-
-    res.render('search-results', {
-      cities,
-      currencies,
-      data: result.data,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
+})
 
 module.exports = router;
 
