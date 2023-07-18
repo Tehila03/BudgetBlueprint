60f83fceb1c280675f694da6f68c2d569858cb39
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-07-18 19:03:37 -0400

    exported cost of living js, imported data into db

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
+const CostOfLiving = require('./costOfLiving');
 // const Project = require('./Project');
 
 // User.hasMany(Project, {
@@ -10,4 +11,4 @@ const User = require('./User');
 //   foreignKey: 'user_id'
 // });
 
-module.exports = { User };
+module.exports = { User, CostOfLiving };
