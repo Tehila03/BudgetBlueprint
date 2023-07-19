ce6794618ea4049bb293dbe57f663607337e0d53
Mikayla <user@users-MBP.attlocal.net>
2023-07-19 19:09:44 -0400

    centered search elements in homepage

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const searchRoutes = require('./searchRoutes');
-const signupRoutes= require('./signupRoutes')
 
-router.use('/users', userRoutes);
+const userRoutes = require('./userRoutes');
+
+router.use('/users',userRoutes);
 router.use('/search', searchRoutes);
 
 module.exports = router;
