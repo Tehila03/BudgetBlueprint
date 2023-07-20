b233fdf42c47d9ed37c782ae7017fdbffcb33ee0
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-07-19 23:34:06 -0400

    attempted to append results to homepage, code failed

--- public/js/search.js
+++ public/js/search.js
@@ -1,38 +1,37 @@
 const SearchHandler = async (event) => {
 
-    const city1 = document.querySelector('#searchinput').value.trim();
-    const city2 = document.querySelector('#futureinput').value.trim();
-    if (city1 && city2) {
-      const response = await fetch(`/api/search`, {
-        method: 'POST',
-        body: JSON.stringify({city1,city2}),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        const result = calculateDifference(data.homeCity, data.destinationCity);
-        console.log(result);
-        let HTML = ``
-        
+  const city1 = document.querySelector('#searchinput').value.trim();
+  const city2 = document.querySelector('#futureinput').value.trim();
+  if (city1 && city2) {
+    const response = await fetch(`/api/search`, {
+      method: 'POST',
+      body: JSON.stringify({ city1, city2 }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const result = calculateDifference(data.homeCity, data.destinationCity);
+      console.log(result);
+      let HTML = ``
+
       // attach response to handlebars**
-      } else {
-        alert('No search results.');
-      }
+    } else {
+      alert('No search results.');
     }
-  };
-  function calculateDifference(city1, city2){
-    const homeCity = city1.col_index
-    const destinationCity = city2.col_index
-    const result = ((destinationCity - homeCity)/homeCity)*100
-    return result
   }
- 
-  
-  document
-    .querySelector('#searchbtn')
-    .addEventListener('click', SearchHandler);
- 
+};
+function calculateDifference(city1, city2) {
+  const homeCity = city1.col_index
+  const destinationCity = city2.col_index
+  const result = ((destinationCity - homeCity) / homeCity) * 100
+  return result
+};
+
+document
+  .querySelector('#searchbtn')
+  .addEventListener('click', SearchHandler);
+
