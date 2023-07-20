c59358add688ab8e393d30e24970a42d5298a33f
Kim Turboutique <127644189+KimTurboutique@users.noreply.github.com>
2023-07-20 00:27:00 -0400

    corrected code in search js file appended search results to homepage

--- public/js/search.js
+++ public/js/search.js
@@ -16,6 +16,8 @@ const SearchHandler = async (event) => {
       console.log(data);
       const result = calculateDifference(data.homeCity, data.destinationCity);
       console.log(result);
+      const resultDiv = document.querySelector('#result');
+      resultDiv.innerHTML = `Based on your search, if you would like to move from your home city to a new city you would need to increase your income by ${result}% in order to sustain and match the cost of living in your new home city.`;
       let HTML = ``
 
       // attach response to handlebars**
@@ -28,10 +30,10 @@ function calculateDifference(city1, city2) {
   const homeCity = city1.col_index
   const destinationCity = city2.col_index
   const result = ((destinationCity - homeCity) / homeCity) * 100
-  return result
+  return result.toFixed(2);
 };
 
-document
-  .querySelector('#searchbtn')
-  .addEventListener('click', SearchHandler);
+document.querySelector('#searchbtn').addEventListener('click', SearchHandler);
+
+
 
